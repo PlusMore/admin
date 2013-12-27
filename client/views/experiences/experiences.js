42183d46a396c04107d4ef805b870f0f71b36cf5
Patrick Scott <patrick.lee.scott@gmail.com>
2013-12-27 15:08:03 -0500

    animations

--- client/views/experiences/experiences.js
+++ client/views/experiences/experiences.js
@@ -22,6 +22,35 @@ Template.experiences.events({
   'click .experience-thumbnail-container': function(e) {
     e.preventDefault();
 
-    $(e.currentTarget).toggleClass('selected');
+    var rect = e.currentTarget.getBoundingClientRect(),
+        $currentTarget = $(e.currentTarget);
+
+    if (!$currentTarget.hasClass('selected')) {
+      $currentTarget.addClass('selected');
+      $currentTarget.removeClass('unselected');
+
+      $(e.currentTarget)
+        .css('position', 'absolute')
+        .css('top', rect.top)
+        .css('right', rect.right)
+        .css('bottom', rect.bottom)
+        .css('left', rect.left);
+    }
+    else {
+      $currentTarget.addClass('unselected');
+      $currentTarget.removeClass('selected');
+
+      $currentTarget.bind("webkitTransitionEnd oTransitionEnd otransitionend transitionend msTransitionEnd", function(){
+        alert('hi');
+      });
+//      TODO: on animationend event remove .css() props
+      $(e.currentTarget)
+        .css('position', '')
+        .css('top', '')
+        .css('right', '')
+        .css('bottom', '')
+        .css('left', '');
+    }
+
   }
 });
