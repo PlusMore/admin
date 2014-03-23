8ab96483c3ae30bf62d1ff7db41b37a11d99db67
Patrick Scott <patrick.lee.scott@gmail.com>
2014-03-23 15:45:41 -0400

    remove timepicker thing

--- client/views/patron-interface/experience/actions/makeReservation.js
+++ client/views/patron-interface/experience/actions/makeReservation.js
@@ -35,10 +35,6 @@ Template.makeReservationForm.helpers({
   }
 });
 
-Template.makeReservationForm.rendered = function () {
-  $(this.find('.time input')).timepicker();
-};
-
 Handlebars.registerHelper("hourOptions", function() {
   var hours = [1,2,3,4,5,6,7,8,9,10,11,12];
   var hourOptions = [];
