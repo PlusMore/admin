34494291b6d49e4d3109654d14864f169e517cb0
Patrick Scott <patrick.lee.scott@gmail.com>
2014-06-10 23:57:09 -0400

    add to experience on add from ui

--- client/views/admin-only/manage-experiences/tagInput.js
+++ client/views/admin-only/manage-experiences/tagInput.js
@@ -7,14 +7,38 @@ Template.tagInput.helpers({
 Template.tagInput.rendered = function () {
   var that = this;
   this.$('.tag-input').selectize({
-    create: function(input) {
-      var tag = Experiences.addTag(input, {_id: that.data._id});
-      debugger;
-      return {
-        text: input,
-        value: input
-      };
+    valueField: 'name',
+    labelField: 'name',
+    searchField: ['name'],
+    create: function(input, cb) {
+      console.log('create tag: ', input)
+      Experiences.addTag(input, {_id: that.data._id});
+      if (cb) {
+        cb(input);
+
+      } 
+
+      return Meteor.tags.findOne({collection: 'experiences', name: input});
     },
-    options: Meteor.tags.find({collection: 'experiences'}).fetch()
+    options: Meteor.tags.find({collection: 'experiences'}).fetch(),
+    render: {
+        item: function(item, escape) {
+            return '<div>' +
+              (item.name ? '<span class="name">' + escape(item.name) + '</span>' : '') +
+            '</div>';
+        },
+        option: function(item, escape) {
+            var name = item.name;
+            var caption = item.nRefs;
+            return '<div>' +
+                '<span class="name">' + escape(name) + '</span>&nbsp;' +
+                (caption ? '<span class="badge">' + escape(caption) + '</span>' : '') +
+            '</div>';
+        }
+    },
+    onItemAdd: function(value, $item) {
+      console.log('add tag: ', value);
+      Experiences.addTag(value, {_id: that.data._id});
+    }
   });
 };
