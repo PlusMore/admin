601b3be97cce38815c3308521e91b5eaaffddf48
Patrick Scott <patrick.lee.scott@gmail.com>
2014-07-01 23:16:01 -0400

    remove debugger

--- server/accounts.js
+++ server/accounts.js
@@ -4,8 +4,6 @@ Accounts.emailTemplates.enrollAccount.subject = function (user) {
     return "Welcome to Plus More";
 };
 Accounts.emailTemplates.enrollAccount.text = function (user, url) {
-  debugger;
-
   // if roles has hotel-staff or hotel-manager then send to hotel app
   if (user.roles.indexOf('hotel-staff') > -1 || user.roles.indexOf('hotel-manager') > -1) {
     var spliturl = url.split('/#');
